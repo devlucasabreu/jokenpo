2995a42d8370d5c4ed0e02a4df2892f4ed25cf1a
devlucasabreu <devlucasmiguelabreu@gmail.com>
2026-04-18 19:35:13 -0300

    criação do script

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
- 
+const result = document.querySelector('.result')
+const yourScore = document.querySelector('#human')
+const machineScore = document.querySelector('#machine')
+
+let humanScoreNumber = 0
+let machineScoreNumber = 0
+
+const playHuman = (humanChoice) => {
+  playTheGame(humanChoice, playMachine())
+}
+
+const playMachine = () => {
+  const choices = ['rock', 'paper', 'scissors']
+
+  const randomNumber = Math.floor(Math.random() * 3)
+
+  return choices[randomNumber]
+}
+
+const playTheGame = (human, machine) => {
+  if (human === machine) {
+    result.innerHTML = 'Deu empate!'
+  } else if (
+    (human === 'paper' && machine === 'rock') ||
+    (human === 'rock' && machine === 'scissors') ||
+    (human === 'scissors' && machine === 'paper')
+  ) {
+    humanScoreNumber++
+    yourScore.innerHTML = humanScoreNumber
+
+    result.innerHTML = 'Você ganhou!'
+  } else {
+    machineScoreNumber++
+    machineScore.innerHTML = machineScoreNumber
+    result.innerHTML = 'Você perdeu!'
+  }
+
+  console.log('Humano: ' + human + ' Maquina: ' + machine)
+}
